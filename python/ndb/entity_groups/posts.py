7b8b547f7289778296fdddb83453ef2caf19ab62
Eli Bixby <elibixby@google.com>
2015-05-01 14:30:42 -0700

    Slight refactoring

--- python/ndb/entity_groups/posts.py
+++ python/ndb/entity_groups/posts.py
@@ -19,21 +19,23 @@ class Post(ndb.Model):
     content = ndb.TextProperty()
     posted = ndb.DateTimeProperty(auto_now_add=True)
 
+
 def make_post(user_id, post_text):
     """ put a post made by the given user, with the given test in datastore"""
     post = Post(key=ndb.Key('User', user_id, 'Post'), 
             content = post_text)
     post.put()
 
-def get_posts(user_id=None, since=datetime.min):
-    """ Retrieve all posts since specified date time. If user_id is specified,
-    retrieves only posts by that user, in a strongly consistent manner """
-    if user_id is None:
-        """ eventually consistent """
-        return Account.query(Account.posted >= since).order(-Account.posted)
-    else:
-        """ strongly consistent """
-        return Account.query(
-                parent = ndb.Key('User', user_id),
-                Account.posted >= since).order(-Account.posted)
 
+def get_posts(since=datetime.min):
+    """ Retrieve all posts since specified datetime. """ 
+    """ eventually consistent """
+    return Account.query(Account.posted >= since).order(-Account.posted)
+
+
+def get_user_posts(user_id, since=datetime.min):
+    """ Retrieves all posts by specified user since specified datetime """
+    """ strongly consistent """
+    return Account.query(
+            parent = ndb.Key('User', user_id),
+            Account.posted >= since).order(-Account.posted)
